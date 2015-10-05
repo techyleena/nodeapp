0807c6f5d30f127e0ebecf69c59a77061948f4d0
Leena <techyleena@gmail.com>
2015-10-05 17:46:28 +0530

    Fixed lint errors

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ var toAdd = 23
 
 function summer (sumVal) {
   return function (toAdd) {
-    if (!toAdd) throw new Error('trying to pull a fast one?')
-    return sumVal + toAdd
+    if (!toAdd) {
+      throw new Error('trying to pull a fast one?');
+    }
+    return sumVal + toAdd;
   }
 }
 
-module.exports = summer
+module.exports = summer;
