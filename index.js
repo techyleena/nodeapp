6e94e921901971f15aa62d1fbe407ca75bf1f12f
Leena <techyleena@gmail.com>
2015-10-06 15:09:30 +0530

    refined index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-var toAdd = 23
+"use strict";
+
+var toAdd = 23;
 
 function summer (sumVal) {
   return function (toAdd) {
     if (!toAdd) {
-      throw new Error('trying to pull a fast one?');
+      throw new Error("trying to pull a fast one?");
     }
     return sumVal + toAdd;
   }
